37b490e4a1fb5d03e5f97407755e135b42967b1a
dstokesy <danielstokes93@gmail.com>
2023-01-02 16:19:08 +0000

    adds content

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,9 +26,61 @@ export default function Index() {
           </h1>
         </div>
       </header>
-      <div className="container-fluid">
-        
-      </div>
+
+      <section className="pt-24">
+        <div className="container">
+          <div className="pb-12">
+            <div className="grid grid-cols-12 gap-4">
+              <div className="col-start-1 col-end-13 sm:col-start-1 sm:col-end-5">
+                <h2 className={styles.heading_two}>Projects</h2>
+              </div>
+              <div className="col-start-1 col-end-13 sm:col-start-5 sm:col-end-13">
+                <p>Proin eget tortor risus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis porttitor volutpat. Donec rutrum congue leo eget malesuada. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
+              </div>
+            </div>
+          </div>
+
+          <hr/>
+
+          <div className="pt-12 pb-12">
+            <div className="grid grid-cols-12 gap-4">
+              <div className="col-start-1 col-end-13 sm:col-start-1 sm:col-end-5">
+                <h2 className={styles.heading_two}>About Me</h2>
+              </div>
+              <div className="col-start-1 col-end-13 sm:col-start-5 sm:col-end-13">
+                <p>Proin eget tortor risus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis porttitor volutpat. Donec rutrum congue leo eget malesuada. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
+              </div>
+            </div>
+          </div>
+
+          <hr/>
+
+          <div className="pt-12 pb-12">
+            <div className="grid grid-cols-12 gap-4">
+              <div className="col-start-1 col-end-13 sm:col-start-1 sm:col-end-5">
+                <h2 className={styles.heading_two}>Experience</h2>
+              </div>
+              <div className="col-start-1 col-end-13 sm:col-start-5 sm:col-end-13">
+                <p>Proin eget tortor risus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis porttitor volutpat. Donec rutrum congue leo eget malesuada. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
+              </div>
+            </div>
+          </div>
+
+          <hr/>
+
+          <div className="pt-12 pb-12">
+            <div className="grid grid-cols-12 gap-4">
+              <div className="col-start-1 col-end-13 sm:col-start-1 sm:col-end-5">
+                <h2 className={styles.heading_two}>Education</h2>
+              </div>
+              <div className="col-start-1 col-end-13 sm:col-start-5 sm:col-end-13">
+                <p>Proin eget tortor risus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis porttitor volutpat. Donec rutrum congue leo eget malesuada. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </section>
     </Layout>
   )
 }
