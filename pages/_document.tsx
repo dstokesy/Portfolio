d8a146ad8bf90f7e16c3f11b0acc8b6c17d51555
dstokesy <danielstokes93@gmail.com>
2023-01-03 16:50:02 +0000

    fixes linting bug

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Cormorant+Infant:wght@300;400;700&family=Dancing+Script&display=swap" rel="stylesheet" />
       </Head>
       <body>
