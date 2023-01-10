d0b740cf43c36a712e408f80656349f34cb0d901
dstokesy <danielstokes93@gmail.com>
2023-01-10 11:43:35 +0000

    updates to use custom type

--- components/ProjectCard/index.tsx
+++ components/ProjectCard/index.tsx
@@ -2,7 +2,16 @@ import styles from './index.module.scss'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function ProjectCard({ project }: { project: any }) {
+type Project = {
+	priority: number
+	title: string
+	categories: string
+	image: string
+	content: string
+	id: string
+};
+
+export default function ProjectCard({ project }: { project: Project }) {
 	return (
 		<Link href={`/projects/${project.id}`} className="block mb-4 text-center">
 			<Image src={project.image} height={166} width={174} alt="Daniel Stokes" className={styles.img} />
