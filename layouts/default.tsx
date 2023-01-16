d822bc3a4326085ab0024248a01f9a8219a88af2
dstokesy <danielstokes93@gmail.com>
2023-01-16 17:18:58 +0000

    removes legacy code

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
 import styles from './default.module.scss'
 
 export const siteTitle = 'Daniel Stokes Full Stack Web Developer'
@@ -11,7 +9,6 @@ export default function Layout({children}: {
   return (
     <div>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
           content="Daniel Stokes Full Stack Web Developer, Leeds"
